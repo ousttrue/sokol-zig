832406c9096ffa73b7082c81bf653fa613f7467a
ousttrue <ousttrue@gmail.com>
2025-01-22 01:42:21 +0900

    emlink 0.14

--- build.zig
+++ build.zig
@@ -377,9 +377,8 @@ pub fn emLinkStep(b: *Build, options: EmLinkOptions) !*Build.Step.InstallDir {
     // TODO: This is hack to support master and 0.13.0 zig versions. Remove after 0.14.0.
     if (builtin.zig_version.major == 0 and builtin.zig_version.minor < 14) {
         // FIXME: old version, remove after 0.14
-        var it = options.lib_main.root_module.iterateDependencies(options.lib_main, false);
-        while (it.next()) |item| {
-            for (item.module.link_objects.items) |link_object| {
+        for (options.lib_main.root_module.getGraph().modules) |module| {
+            for (module.link_objects.items) |link_object| {
                 switch (link_object) {
                     .other_step => |compile_step| {
                         switch (compile_step.kind) {
